3093cbe838df8889e55235cb8c1938e5a762f3a7
zhangxinmiao <82506588+zhangxinmiao-study@users.noreply.github.com>
2022-06-26 22:56:32 +0800

    init

--- client/.umirc.ts
+++ client/.umirc.ts
@@ -1,35 +1,11 @@
-import { defineConfig } from '@umijs/max';
+import { defineConfig } from 'umi';
 
 export default defineConfig({
-  antd: {},
-  access: {},
-  model: {},
-  initialState: {},
-  request: {},
-  layout: {
-    title: '@umijs/max',
+  nodeModulesTransform: {
+    type: 'none',
   },
   routes: [
-    {
-      path: '/',
-      redirect: '/home',
-    },
-    {
-      name: '首页',
-      path: '/home',
-      component: './Home',
-    },
-    {
-      name: '权限演示',
-      path: '/access',
-      component: './Access',
-    },
-    {
-        name: ' CRUD 示例',
-        path: '/table',
-        component: './Table',
-    },
+    { path: '/', component: '@/pages/index' },
   ],
-  npmClient: 'pnpm',
+  fastRefresh: {},
 });
-
