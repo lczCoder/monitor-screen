356c8bc3cbbc506c2b47fea570d39bd3ac73dea8
lczCoder <lczcoder@163.com>
2022-07-04 00:14:42 +0800

    feat:解析json字符串转interface

--- server/controllers/webtrace/eventContoller.go
+++ server/controllers/webtrace/eventContoller.go
@@ -1,6 +1,7 @@
 package webtrace
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
@@ -11,4 +12,11 @@ func GetParseEvent (ctx *gin.Context){
 
 func PostParseEvent (ctx *gin.Context){
 	fmt.Println("sdk事件上报处理,post请求")
+	var res = ctx.PostForm("opt")
+	var rtype []map[string]interface{}
+	json.Unmarshal([]byte(res), &rtype)
+	for _,v:= range rtype {
+		var z = v["deviceInfo"]
+		fmt.Println(z.(map[string]interface{})["browser"])
+	}
 }
