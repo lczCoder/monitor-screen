faf903a19bbb1076bd8d8a7d786440950467a072
lczCoder <lczcoder@163.com>
2022-07-10 17:06:45 +0800

    feat:新增性能监控表结构

--- server/service/paramsSdk.go
+++ server/service/paramsSdk.go
@@ -8,28 +8,28 @@ import (
 	"monitor/models"
 )
 
-func ParsePostForm (params models.SourceField){
+func ParsePostForm(params models.SourceField) {
 	// 业务代码编写
 	var data []models.SdkParams
 	json.Unmarshal([]byte(params.Opt), &data)
-	fmt.Println("数据解析",data)
-	fmt.Printf("%T",data)
-	for i,v := range data {
+	fmt.Println("数据解析", data)
+	fmt.Printf("%T", data)
+	for i, v := range data {
 		sql := &database.ErrInfo{
-			ID:i+1,
-			Version:v.Version,
-			UUID:v.UUID,
-			Belong:"test",
-			ErrType:v.EventInfo.EventType,
-			ErrTime:v.EventInfo.Time,
-			ErrMsg:v.EventInfo.Info.File,
-			ErrFile:v.EventInfo.Info.File,
-			ErrLine:v.EventInfo.Info.TimeCost,
-			ErrCol:v.EventInfo.Info.TimeCost,
-			ErrObj:v.EventInfo.Info.Type,
-			ErrURL:v.Appid,
+			ID:      i + 1,
+			Version: v.Version,
+			UUID:    v.UUID,
+			Belong:  "test",
+			ErrType: v.EventInfo.EventType,
+			ErrTime: v.EventInfo.Time,
+			ErrMsg:  v.EventInfo.Info.File,
+			ErrFile: v.EventInfo.Info.File,
+			ErrLine: v.EventInfo.Info.TimeCost,
+			ErrCol:  v.EventInfo.Info.TimeCost,
+			ErrObj:  v.EventInfo.Info.Type,
+			ErrURL:  v.Appid,
 		}
-		result:= database.DB.Create(&sql)
-		fmt.Println("sql插入结果",result)
+		result := database.DB.Create(&sql)
+		fmt.Println("sql插入结果", result)
 	}
 }
